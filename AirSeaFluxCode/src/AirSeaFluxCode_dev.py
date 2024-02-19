bugfix(humidity): Ensure humidity units are consistent throughout
All units of humidity are `[g/kg]` throughout AirSeaFluxCode and
associated sub routines. Resolves issue #1.

AirSeaFluxCode/src/AirSeaFluxCode_dev.py
@@ -46,8 +46,8 @@ class S88:
         if (np.all(np.isnan(self.qsea)) or np.all(np.isnan(self.qair))):
             raise ValueError("qsea and qair cannot be nan")
         # if self.meth in ["NCAR", "ecmwf"]:
-        #     self.qair = np.maximum(self.qair, 1e-6)
-        self.dq_in = self.qair-self.qsea
+        #     self.qair = np.maximum(self.qair, 1e-3)  # [g/kg]
+        self.dq_in = self.qair-self.qsea  # [g/kg]
         # if self.meth in ["NCAR", "ecmwf"]:
         #     self.dq_in = np.maximum(np.abs(self.dq_in), 1e-9)*np.sign(
         #         self.dq_in)
@@ -57,10 +57,11 @@ class S88:
         if self.meth == "C35":
             self.cp  = 1004.67*np.ones(self.SST.shape)
         elif self.meth in ["NCAR", "ecmwf"]:
-            self.cp = 1005+1860*self.qair
+            self.cp = 1005+1.860*self.qair  # qair [g/kg]
         else:
-            self.cp = 1004.67*(1+0.00084*self.qsea)
-        self.tlapse = gamma("dry", self.SST, self.T, self.qair/1000, self.cp)
+            self.cp = 1004.67*(1+0.84*self.qsea*0.001)  # qsea [g/kg]
+        # gamma takes humidity as [g/kg]
+        self.tlapse = gamma("dry", self.SST, self.T, self.qair, self.cp)
         self.theta = np.copy(self.T)+self.tlapse*self.h_in[1]
         self.dt_in = self.theta-self.SST
         # if self.meth == "ecmwf":
@@ -148,13 +149,14 @@ class S88:
         self.ref10 = 10
 
         #  first guesses
-        self.t10n, self.q10n = np.copy(self.theta), np.copy(self.qair)
-        self.rho = self.P*100/(287.1*self.t10n*(1+0.6077*self.q10n))
+        self.t10n, self.q10n = np.copy(self.theta), np.copy(self.qair)  # q: [g/kg]
+        self.rho = self.P*100/(287.1*self.t10n*(1+0.6077*self.q10n*0.001))  # q: [g/kg]
         self.lv = (2.501-0.00237*(self.SST-CtoK))*1e6  # J/kg
 
         #  Zeng et al. 1998
         self.tv = self.theta*(1+0.6077*self.qair)   # virtual potential T
-        self.dtv = self.dt_in*(1+0.6077*self.qair)+0.6077*self.theta*self.dq_in
+        self.dtv = self.dt_in*(1+0.6077*self.qair*0.001) + \
+                0.6077*self.theta*self.dq_in*0.001  # q: [g/kg]
 
         # Set the wind array
         self.wind = np.sqrt(np.power(np.copy(self.spd), 2)+0.25)
@@ -204,7 +206,7 @@ class S88:
 
         # Decide which variables to use in tolerances based on tolerance
         # specification
-        tol = ['all', 0.01, 0.01, 1e-05, 1e-3,
+        tol = ['all', 0.01, 0.01, 1e-2, 1e-3,
                0.1, 0.1] if tol is None else tol
         assert tol[0] in ['flux', 'ref', 'all'], "unknown tolerance input"
 
@@ -356,11 +358,11 @@ class S88:
                 self.zUrho = self.wind*np.maximum(self.rho, 1)
                 self.tau = self.zUrho*self.cd*self.spd
                 self.sensible = self.zUrho*self.ct*(self.theta-self.SST)*self.cp
-                self.latent = self.zUrho*self.cq*(self.qair-self.qsea)*self.lv
+                self.latent = self.zUrho*self.cq*(self.qair-self.qsea)*self.lv*0.001  # q: [g/kg]
             else:
                 self.tau = self.rho*np.power(self.usr, 2)*self.spd/self.wind
                 self.sensible = self.rho*self.cp*self.usr*self.tsr
-                self.latent = self.rho*self.lv*self.usr*self.qsr
+                self.latent = self.rho*self.lv*self.usr*self.qsr*0.001  # q: [g/kg]
             # Set the new variables (for comparison against "old")
             new = np.array([np.copy(getattr(self, i)) for i in new_vars])
 
@@ -395,7 +397,7 @@ class S88:
             self.rh = 100*esd/es
         elif self.hum[0] == "q":
             es = 611.21*np.exp(17.502*((self.T-CtoK)/(self.T-32.19)))
-            e = self.qair*self.P/(0.378*self.qair+0.622)
+            e = self.qair*self.P/(0.378*self.qair+622)  # qair: [g/kg]
             self.rh = 100*e/es
 
     def _flag(self, out=0):
@@ -726,7 +728,7 @@ def AirSeaFluxCode_dev(spd, T, SST, SST_fl, meth, lat=None, hum=None, P=None,
            option : 'ref' to set tolerance limits for height adjustment lim-1-3
            option : 'all' to set tolerance limits for both fluxes and height
                     adjustment lim1-6
-           default is tol=['all', 0.01, 0.01, 1e-05, 1e-3, 0.1, 0.1]
+           default is tol=['all', 0.01, 0.01, 1e-2, 1e-3, 0.1, 0.1]
         maxiter : int
             number of iterations (default = 10)
         out : int

AirSeaFluxCode/src/cs_wl_subs.py
@@ -102,7 +102,7 @@ def cs(sst, d, rho, Rs, Rnl, cp, lv, usr, tsr, qsr, grav, opt):
     tcw = 0.6
     Rns = 0.945*Rs  # albedo correction
     shf = rho*cp*usr*tsr
-    lhf = rho*lv*usr*qsr
+    lhf = rho*lv*usr*qsr*0.001  # qsr [g/kg]
     Qnsol = shf+lhf+Rnl
     if opt == "C35":
         cpw = 4000
@@ -181,7 +181,7 @@ def cs_C35(sst, rho, Rs, Rnl, cp, lv, delta, usr, tsr, qsr, grav):
         rho, 2))
     Rns = 0.945*Rs  # albedo correction
     shf = rho*cp*usr*tsr
-    lhf = rho*lv*usr*qsr
+    lhf = rho*lv*usr*qsr*0.001  # qsr [g/kg]
     Qnsol = shf+lhf+Rnl
     fs = 0.065+11*delta-6.6e-5/delta*(1-np.exp(-delta/8.0e-4))
     Q = Qnsol+Rns*fs
@@ -269,7 +269,7 @@ def cs_ecmwf(rho, Rs, Rnl, cp, lv, usr, tsr, qsr, sst, grav):
     aw = np.maximum(1e-5, 1e-5*(sst-CtoK))
     Rns = 0.945*Rs  # (net solar radiation (albedo correction)
     shf = rho*cp*usr*tsr
-    lhf = rho*lv*usr*qsr
+    lhf = rho*lv*usr*qsr*0.001  # qsr [g/kg]
     Qnsol = shf+lhf+Rnl  # eq. 8.152
     d = delta(aw, Qnsol, usr, grav)
     for jc in range(4):  # because implicit in terms of delta...
@@ -333,7 +333,7 @@ def wl_ecmwf(rho, Rs, Rnl, cp, lv, usr, tsr, qsr, sst, skt, dtc, grav):
     b1, b2, b3 = -71.5, -2.8, -0.06  # [m-1]
     Rd = 1-(a1*np.exp(b1*rd0)+a2*np.exp(b2*rd0)+a3*np.exp(b3*rd0))
     shf = rho*cp*usr*tsr
-    lhf = rho*lv*usr*qsr
+    lhf = rho*lv*usr*qsr*0.001  # qsr [g/kg]
     Qnsol = shf+lhf+Rnl
     usrw = np.maximum(usr, 1e-4)*np.sqrt(1.2/rhow)   # u* in the water
     zc3 = rd0*kappa*grav/np.power(1.2/rhow, 3/2)
@@ -404,7 +404,7 @@ def cs_Beljaars(rho, Rs, Rnl, cp, lv, usr, tsr, qsr, grav, Qs):
     aw = 3e-4       # thermal expansion coefficient [K-1]
     Rns = 0.945*Rs  # net solar radiation (albedo correction)
     shf = rho*cp*usr*tsr
-    lhf = rho*lv*usr*qsr
+    lhf = rho*lv*usr*qsr*0.001  # qsr [g/kg]
     Q = Rnl+shf+lhf+Qs
     xt = 16*Q*grav*aw*cpw*np.power(rhow*visw, 3)/(
         np.power(usr, 4)*np.power(rho*tcw, 2))

AirSeaFluxCode/src/hum_subs.py
@@ -304,6 +304,7 @@ def qsat_sea(T, P, qmeth):
     Returns
     -------
     qs : float
+        surface saturation specific humidity [g/kg]
     """
     T = np.asarray(T)
     if np.nanmin(T) > 200:  # if Ta in Kelvin convert to Celsius
@@ -311,7 +312,7 @@ def qsat_sea(T, P, qmeth):
     ex = VaporPressure(T, P, 'liquid', qmeth)
     es = 0.98*ex  # reduction at sea surface
     qs = 622*es/(P-0.378*es)
-    return qs
+    return qs  # [g/kg]
 # -----------------------------------------------------------------------------
 
 
@@ -333,7 +334,7 @@ def qsat_air(T, P, rh, qmeth):
     Returns
     -------
     q : float
-    em : float
+        specific humidity [g/kg]
     """
     T = np.asarray(T)
     if np.nanmin(T) > 200:  # if Ta in Kelvin convert to Celsius
@@ -341,7 +342,7 @@ def qsat_air(T, P, rh, qmeth):
     es = VaporPressure(T, P, 'liquid', qmeth)
     em = 0.01*rh*es
     q = 622*em/(P-0.378*em)
-    return q
+    return q  # [g/kg]
 # -----------------------------------------------------------------------------
 
 
@@ -353,39 +354,37 @@ def get_hum(hum, T, sst, P, qmeth):
     ----------
     hum : array
         humidity input switch 2x1 [x, values] default is relative humidity
-            x='rh' : relative humidity in %
-            x='q' : specific humidity (kg/kg)
-            x='Td' : dew point temperature (K)
+            x='rh' : relative humidity [%]
+            x='q' : specific humidity [g/kg]
+            x='Td' : dew point temperature [K]
     T : float
-        air temperature in K
+        air temperature [K]
     sst : float
-        sea surface temperature in K
+        sea surface temperature [K]
     P : float
-        air pressure at sea level in hPa
+        air pressure at sea level [hPa]
     qmeth : str
         method to calculate specific humidity from vapor pressure
 
     Returns
     -------
     qair : float
-        specific humidity of air
+        specific humidity of air [g/kg]
     qsea : float
-        specific humidity over sea surface
+        specific humidity over sea surface [g/kg]
 
     """
-    if (hum[0] not in ['rh', 'q', 'Td', 'no']):
-        sys.exit("unknown humidity input")
-        qair, qsea = np.nan, np.nan
-    elif ((hum[0] == 'rh') or (hum[0] == 'no')):
+    if ((hum[0] == 'rh') or (hum[0] == 'no')):
         RH = hum[1]
         if np.all(RH < 1):
-            sys.exit("input relative humidity units should be %")
-            qair, qsea = np.nan, np.nan
-        qsea = qsat_sea(sst, P, qmeth)/1000    # surface water q (kg/kg)
-        qair = qsat_air(T, P, RH, qmeth)/1000  # q of air (kg/kg)
+            raise ValueError("input relative humidity units should be %")
+        qsea = qsat_sea(sst, P, qmeth)  # surface water q [g/kg]
+        qair = qsat_air(T, P, RH, qmeth)  # q of air [g/kg]
     elif hum[0] == 'q':
-        qair = hum[1]
-        qsea = qsat_sea(sst, P, qmeth)/1000  # surface water q (kg/kg)
+        qair = hum[1]  # [g/kg]
+        if np.all(qair < 1):
+            raise ValueError("input humidity units should be g/kg")
+        qsea = qsat_sea(sst, P, qmeth)  # surface water q [g/kg]
     elif hum[0] == 'Td':
         Td = hum[1]  # dew point temperature (K)
         Td = np.where(Td < 200, np.copy(Td)+CtoK, np.copy(Td))
@@ -393,8 +392,10 @@ def get_hum(hum, T, sst, P, qmeth):
         esd = 611.21*np.exp(17.502*((Td-273.16)/(Td-32.19)))
         es = 611.21*np.exp(17.502*((T-273.16)/(T-32.19)))
         RH = 100*esd/es
-        qair = qsat_air(T, P, RH, qmeth)/1000  # q of air (kg/kg)
-        qsea = qsat_sea(sst, P, qmeth)/1000    # surface water q (kg/kg)
+        qair = qsat_air(T, P, RH, qmeth)  # q of air [g/kg]
+        qsea = qsat_sea(sst, P, qmeth)    # surface water q [g/kg]
+    else:
+        raise ValueError('(get_hum) Unknown humidity input')
     return qair, qsea
 # -----------------------------------------------------------------------------
 
@@ -414,7 +415,7 @@ def gamma(opt, sst, t, q, cp):
     t : float
         air temperature [K]
     q : float
-        specific humidity of air [kg/kg]
+        specific humidity of air [g/kg]
     cp : float
         specific capacity of air at constant Pressure
 
@@ -424,6 +425,7 @@ def gamma(opt, sst, t, q, cp):
         lapse rate [K/m]
 
     """
+    q = np.copy(q) / 1000  # convert to [kg/kg]
     if np.nanmin(sst) < 200:  # if sst in Celsius convert to Kelvin
         sst = sst+CtoK
     if np.nanmin(t) < 200:  # if t in Celsius convert to Kelvin

AirSeaFluxCode/src/util_subs.py
@@ -110,7 +110,7 @@ def set_flag(miss, rh, u10n, q10n, t10n, Rb, hin, monob, itera, out=0):
     u10n : float
         10m neutral wind speed        [ms^{-1}]
     q10n : float
-        10m neutral specific humidity [kg/kg]
+        10m neutral specific humidity [g/kg]
     t10n : float
         10m neutral air temperature   [K]
     Rb : float
@@ -131,7 +131,7 @@ def set_flag(miss, rh, u10n, q10n, t10n, Rb, hin, monob, itera, out=0):
     """
     # set maximum/minimum acceptable values
     u10max = 200
-    q10max = 40*0.001
+    q10max = 40  # [g/kg] (Equivalent to 0.04 kg/kg)
     t10min, t10max = 173, 373
     Rbmin, Rbmax = -0.5, 0.2
     flag = np.full(miss.shape, "n", dtype="object")
@@ -231,7 +231,7 @@ def rho_air(T, qair, p):
     T : float
         absolute air temperature             [K]
     qair : float
-        air specific humidity   [kg/kg]
+        air specific humidity   [g/kg]
     p : float
         pressure in                [Pa]
 
@@ -241,5 +241,5 @@ def rho_air(T, qair, p):
         density of moist air   [kg/m^3]
 
     """
-    rho_air = np.maximum(p/(287.05*T*(1+(461.495/287.05-1)*qair)), 0.8)
+    rho_air = np.maximum(p/(287.05*T*(1+(461.495/287.05-1)*qair*0.001)), 0.8)
     return rho_air